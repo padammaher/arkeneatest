4097407fd88e48ff343f6006c60caf59cdce7b84
Padam <padamasing.m@mindworx.in>
2018-09-14 19:25:05 +0530

    application\modules\AssetsManagement\views\Assets\assets_list.php

--- application/modules/AssetsManagement/views/Trigger/trigger_add.php
+++ application/modules/AssetsManagement/views/Trigger/trigger_add.php
@@ -8,45 +8,72 @@
                     <div class="clearfix"></div>
                 </div>
                 <div class="x_content">
-                    <form class="form-horizontal form-label-left">
+                    
+<?php // print_r($threshold_array); 
+$trigger_edit_data=array('trigger_name'=>'','trigger_threshold_id'=>'','email'=>'','sms_contact_no'=>'','id'=>'');
+foreach ($trigger_edit_list as $trigger_edit_data) {
+ }  ?>
+                    <form class="form-horizontal form-label-left" action="<?php echo base_url() ?>trigger_add" method="POST">
                         <div class="item form-group">
                             <label class="control-label col-md-3 col-sm-3 col-xs-12">Alarm Trigger Name</label>
                             <div class="col-md-6 col-sm-6 col-xs-12">
-                                <input type="text" class="form-control" placeholder="Extreme Oil Pressure" required="required">
+                                <?php // print_r($this->input->post()); ?>
+                                <input type="text" name="trigger_name" value="<?php echo set_value('trigger_name',$trigger_edit_data['trigger_name']);?>" class="form-control" placeholder="Extreme Oil Pressure" required="required">
                             </div>
                         </div>
 
                         <div class="item form-group">
                             <label class="control-label col-md-3 col-sm-3 col-xs-12">Trigger Threshold</label>
                             <div class="col-md-6 col-sm-6 col-xs-12">
-                                <select class="form-control">
-                                    <option value="10">Green</option>
-                                    <option value="20">Red</option>
-                                    <option value="30">Orange</option>
-
-                                </select>
+                                <select name="trigger_threshold" class="form-control" required>  
+                                    <option value="">Select Type</option>
+                                    <?php if(isset($threshold_array) && !empty($threshold_array)){
+                                            if(!in_array('Green',$threshold_array)){ ?>
+                                                <option value="Green" <?php echo (set_value('trigger_threshold'))=='Green' ?'selected':($trigger_edit_data['trigger_threshold_id'])=='Green' ?'selected':'';  ?>>Green</option>
+                                           <?php }
+                                            if(!in_array('Red',$threshold_array)){ ?>
+                                                <option value="Red" <?php echo (set_value('trigger_threshold'))=='Red' ?'selected':($trigger_edit_data['trigger_threshold_id'])=='Red' ?'selected':'';  ?>>Red</option>
+                                            <?php }
+                                            if(!in_array('Orange',$threshold_array)){ ?>
+                                                <option value="Orange" <?php echo (set_value('trigger_threshold'))=='Orange' ?'selected':($trigger_edit_data['trigger_threshold_id'])=='Orange' ?'selected':'';  ?>>Orange</option>
+                                           <?php }
+                                           if (!empty($trigger_edit_data['trigger_threshold_id'])) { ?>  
+                            <option value="<?php echo $trigger_edit_data['trigger_threshold_id'];?>" <?php echo (set_value('trigger_threshold'))== $trigger_edit_data['trigger_threshold_id'] ?'selected':'selected';  ?> ><?php echo $trigger_edit_data['trigger_threshold_id'];?></option>   
+                                        <?php }
+                                        }else {  ?>
+                                    <option value="Green" <?php echo (set_value('trigger_threshold'))=='Green' ?'selected':($trigger_edit_data['trigger_threshold_id'])=='Green' ?'selected':'';  ?>>Green</option>
+                                    <option value="Red" <?php echo (set_value('trigger_threshold'))=='Red' ?'selected':($trigger_edit_data['trigger_threshold_id'])=='Red' ?'selected':'';  ?>>Red</option>
+                                    <option value="Orange" <?php echo (set_value('trigger_threshold'))=='Orange' ?'selected':($trigger_edit_data['trigger_threshold_id'])=='Orange' ?'selected':'';  ?>>Orange</option>
+                                        <?php } ?></select>
                             </div>
                         </div>
 
                         <div class="item form-group">
-                            <label class="control-label col-md-3 col-sm-3 col-xs-12"><input type="checkbox"  onclick="undisable()"> Email</label>
+                            <label class="control-label col-md-3 col-sm-3 col-xs-12"><input type="checkbox"  onclick="undisable(this);" name="check_email" id="check_email" <?php echo (set_value('check_email'))== 'on'? 'checked':''; ?> > Email</label>
                             <div class="col-md-6 col-sm-6 col-xs-12">
-                                <input type="text" class="form-control" placeholder="abc@xyz.com" id="myCheck" disabled>
+                                <?php ?>
+                                <input type="text" name="email" id="email" value="<?php echo set_value('email',$trigger_edit_data['email']);?>" class="form-control" placeholder="abc@xyz.com" <?php if(!empty(set_value('check_email'))){ echo (set_value('check_email'))== 'on'? '':'readonly';} else{ echo 'readonly';} ?> >
                             </div>
                         </div>
 
                         <div class="item form-group">
-                            <label class="control-label col-md-3 col-sm-3 col-xs-12"><input type="checkbox"  onclick="undisable1()"> SMS</label>
+                            <label class="control-label col-md-3 col-sm-3 col-xs-12"><input type="checkbox"  onclick="undisable1(this)" name="check_contact" id="check_contact" <?php echo (set_value('check_contact'))== 'on'? 'checked':''; ?>> SMS</label>
                             <div class="col-md-6 col-sm-6 col-xs-12">
-                                <input type="text" class="form-control" id="myCheck1" placeholder="9823230011" disabled>
+                                <input type="text" name="contactno" value="<?php echo set_value('contactno',$trigger_edit_data['sms_contact_no']);?>" class="form-control" id="contactno" placeholder="9823230011" <?php if(!empty(set_value('check_contact'))){ echo (set_value('check_contact'))== 'on'? '':'readonly';} else{ echo 'readonly';} ?>>
                             </div>
                         </div>
 
                         <div class="ln_solid"></div>
                         <div class="item form-group">
                             <div class="col-md-6 col-sm-6 col-xs-12 col-md-offset-3">
+                            <?php if(!empty($trigger_edit_list)) { ?>
+                                <input type="hidden" name="trigger_form_action"  value="update" />
+                                <input type="hidden" value="<?php echo $trigger_edit_data['id']; ?>" name="trigger_post_id"/>
+                            <?php } else {?>                                
+                                <input type="hidden" name="trigger_form_action"  value="add" />
+                            <?php } ?>    
                                 <button type="submit" class="btn btn-primary">Save</button>
-                                <button type="button" class="btn btn-default">Cancel</button>
+                                 <a href="<?php echo base_url('trigger_list'); ?>" type="button" class="btn btn-default">Cancel</a>
 
                             </div>
                         </div>
@@ -56,3 +83,26 @@
         </div>
     </div>
 </div>
+<script src="<?php echo base_url(); ?>assets/jquery/jquery-3.1.1.js"></script>
+<script type="text/javascript">
+function undisable(vall) {
+    if(vall.checked == true){
+    $("#email").attr("readonly", false);
+    }
+    else if(vall.checked == false)
+    {
+        $("#email").attr("readonly", true);
+    }
+}
+
+function undisable1(vall) {
+//    document.getElementById("contactno").disabled = false;
+    if(vall.checked == true){
+    $("#contactno").attr("readonly", false);
+    }
+    else if(vall.checked == false)
+    {
+        $("#contactno").attr("readonly", true);
+    }
+}
+</script>
--- application/modules/AssetsManagement/views/Trigger/trigger_list.php
+++ application/modules/AssetsManagement/views/Trigger/trigger_list.php
@@ -30,15 +30,15 @@
 
                     <div class="title_right">
                         <div class="pull-right">
-                            <a href="assets-list.html" class="btn btn-sm btn-primary"> <i class="fa fa-arrow-left"></i> Asset Management</a>
-                            <a href="asset-parameter-range.html" class="btn btn-sm btn-primary"> <i class="fa fa-arrow-left"></i> Asset Parameter Range List</a>
-                            <a href="rule-action-master-list.html" class="btn btn-sm btn-primary"> <i class="fa fa-arrow-left"></i> Rule & Action Master List</a>
-                            <a href="trigger-add.html" class="btn btn-sm btn-primary"> <i class="fa fa-plus"></i> Add Trigger</a>
+                            <a href="<?php echo base_url('Assets_list');?>" class="btn btn-sm btn-primary"> <i class="fa fa-arrow-left"></i> Asset Management</a>
+                            <a href="<?php echo base_url('asset_parameter_range_list');?>" class="btn btn-sm btn-primary"> <i class="fa fa-arrow-left"></i> Asset Parameter Range List</a>
+                            <a href="<?php echo base_url('AssetsManagement/asset_rule_list');?>" class="btn btn-sm btn-primary"> <i class="fa fa-arrow-left"></i> Rule & Action Master List</a>
+                            <a class="btn btn-sm btn-primary trigger_add"> <i class="fa fa-plus"></i> Add Trigger</a>
                         </div>
                     </div>
                 </div>
 
-                <div class="x_content">
+                <div class="x_content" id="trigger-list">
 
                     <div class="row clearfix">
                         <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
@@ -52,47 +52,44 @@
                             </ul>
                         </div>
                     </div>
-
-                    <div class="row clearfix">
+<?php $i=1;
+if (!empty($trigger_list)){ foreach ($trigger_list as $trigger_list_data) { ?>
+                     <div class="row clearfix">
                         <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
                             <ul class="flex-container flex-item_row nowrap">
-                                <li class="flex-item">1</li>
-                                <li class="flex-item">Extreme Oil Pressure</li>
-                                <li class="flex-item">Red (12 bar)</li>
-                                <li class="flex-item">abc@xyz.com</li>
-                                <li class="flex-item">9823230011</li>
+                                <li class="flex-item"><?php echo $i;?></li>
+                                <li class="flex-item"><?php echo ($trigger_list_data['trigger_name'])== ''? '<i class="fa fa-times" aria-hidden="true"></i>':$trigger_list_data['trigger_name']; ?></li>
+                                <li class="flex-item"><?php echo ($trigger_list_data['trigger_threshold_id'])== ''? '<i class="fa fa-times" aria-hidden="true"></i>':$trigger_list_data['trigger_threshold_id']; ?></li>
+                                <li class="flex-item"><?php echo ($trigger_list_data['email'])== ''? '<i class="fa fa-times" aria-hidden="true"></i>':$trigger_list_data['email'];?> </li>
+                                <li class="flex-item"><?php echo ($trigger_list_data['sms_contact_no'])== ''? '<i class="fa fa-times" aria-hidden="true"></i>':$trigger_list_data['sms_contact_no'];?></li>
                                 <li class="flex-item">
-                                    <a href="trigger-edit.html" title="Edit">
-                                        <i class="fa fa-pencil blue" data-toggle="tooltip" data-placement="top" title="" data-original-title="Edit"></i>
-                                    </a>
-                                    <a href="#" title="Delete">
-                                        <i class="fa fa-trash red" data-toggle="tooltip" data-placement="top" title="" data-original-title="Delete"></i>
-                                    </a>
+                                    
+            <form action="<?php echo base_url(); ?>trigger_add" method="post" id="trigger_form<?php echo $i; ?>">
+                <input type="hidden" id="triggerid<?php echo $i; ?>" value="<?php echo $trigger_list_data['id']; ?>" name="trigger_post_id"/>
+                <input type="hidden" name="trigger_form_action" id="trigger_form_action<?php echo $i; ?>"/>
+                <a title="Edit" class="edit" id="<?php echo $i; ?>">  
+                    <i class="fa fa-pencil blue" data-toggle="tooltip" data-placement="top" title="" data-original-title="Edit"></i>
+                </a>
+                <a title="Delete" class="delete" id="<?php echo $i; ?>">
+                    <i class="fa fa-trash red" data-toggle="tooltip" data-placement="top" title="" data-original-title="Delete"></i> 
+                </a> 
+            </form>
                                 </li>
                             </ul>
                         </div>
                     </div>
-
+<?php $i++; } }else{ ?>
                     <div class="row clearfix">
                         <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
                             <ul class="flex-container flex-item_row nowrap">
-                                <li class="flex-item">2</li>
-                                <li class="flex-item">Moderate Oil Pressure</li>
-                                <li class="flex-item">Orange (9 bar)</li>
-                                <li class="flex-item">abc@xyz.com</li>
-                                <li class="flex-item"><i class="fa fa-times" aria-hidden="true"></i></li>
-                                <li class="flex-item">
-                                    <a href="trigger-edit.html" title="Edit">
-                                        <i class="fa fa-pencil blue" data-toggle="tooltip" data-placement="top" title="" data-original-title="Edit"></i>
-                                    </a>
-                                    <a href="#" title="Delete">
-                                        <i class="fa fa-trash red" data-toggle="tooltip" data-placement="top" title="" data-original-title="Delete"></i>
-                                    </a>
-                                </li>
+                                
+                                <li class="flex-item">data not found..!</li>
+                                
+                                
                             </ul>
                         </div>
                     </div>
-
+<?php } ?>
                     <ul class="pagination">
                         <li><a href="#">1</a></li>
                         <li class="active"><a href="#">2</a></li>
@@ -106,3 +103,36 @@
         </div>
     </div>
 </div>
+
+<script src="<?php echo base_url(); ?>assets/jquery/jquery-3.1.1.js"></script>
+<script type="text/javascript">
+    $(document).ready(function () {
+        $(".edit").click(function () {
+            var id = $(this).attr('id');
+            $("#trigger_form_action" + id).val('edit');
+//            alert(id);
+            $("#trigger_form" + id).submit();
+        });
+        
+                $(".delete").click(function () {
+            var flag = confirm('Are you sure you want to delete this item?');
+            if (flag == true) {
+                var id = $(this).attr('id');
+                $("#trigger_form_action" + id).val('delete');
+                $("#trigger_form" + id).submit();
+            }
+        });
+        
+        
+         $(".trigger_add").click(function () {
+           
+                var id = 1;
+                $("#trigger_form_action"+id).val('addNew');
+                $("#triggerid"+id).val('');
+//                alert($("#triggerid"+id).val());
+                $("#trigger_form" + id).submit();
+           
+        }); 
+      });
+        
+</script>        
