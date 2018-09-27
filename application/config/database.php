3143e14ea48b01fda86a4293ce0447871d56d037
Swapnil Barhate <swapnil.b@mindworx.in>
2018-09-27 14:23:11 +0530

    application\config\database.php

--- application/config/database.php
+++ application/config/database.php
@@ -75,28 +75,6 @@ $active_group = 'default';
 $query_builder = TRUE;
 
 $db['default'] = array(
-<<<<<<< .mine
-	'dsn'	=> '',
-	'hostname' => '1',
-	'hostname' => '192.168.1.45',
-	'username' => 'admin',
-	'password' => 'root',
-	'database' => 'framework',
-	'dbdriver' => 'mysqli',
-	'dbprefix' => '',
-	'pconnect' => FALSE,
-	'db_debug' => (ENVIRONMENT !== 'production'),
-	'cache_on' => FALSE,
-	'cachedir' => '',
-	'char_set' => 'utf8',
-	'dbcollat' => 'utf8_general_ci',
-	'swap_pre' => '',
-	'encrypt' => FALSE,
-	'compress' => FALSE,
-	'stricton' => FALSE,
-	'failover' => array(),
-	'save_queries' => TRUE
-=======
     'dsn' => '',
     'hostname' => '1',
     'hostname' => '192.168.1.45',
@@ -117,5 +95,4 @@ $db['default'] = array(
     'stricton' => FALSE,
     'failover' => array(),
     'save_queries' => TRUE
->>>>>>> .theirs
 );
