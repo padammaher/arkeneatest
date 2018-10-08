0e2965669de5a6bfc22fbdc61f75ba7eb2e3c614
Swapnil Barhate <swapnil.b@mindworx.in>
2018-10-08 14:28:34 +0530

    application\views\403.php assets\images\403.png

---
+++ application/views/403.php
@@ -0,0 +1,20 @@
+<div class="">
+    <div class="clearfix"></div>
+    <div class="row">
+        <!-- page content -->
+        <div class="col-md-12">
+            <div class="col-middle">
+                <div class="col-md-4"></div>
+                <div class="text-center text-center col-md-4">
+                    <p><img src="<?php echo base_url('assets/images/403.png'); ?>" class="img-responsive"></p>
+                    <h1 class="error-number">403</h1>
+                    <h2>Access denied</h2>
+                    <p>We're sorry, you don't have access to the page you requested. </a>
+                    </p>
+                </div>
+                <div class="col-md-4"></div>
+            </div>
+        </div>
+        <!-- /page content -->
+    </div>
+</div>
