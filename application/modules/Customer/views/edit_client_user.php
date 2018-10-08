f5b63b389cae2850922dbf5befbb781d47bf03dc
Pradnya <pradnya.w@mindworx.in>
2018-10-08 11:23:04 +0530

    application\modules\Customer\views\edit_client_user.php

--- application/modules/Customer/views/edit_client_user.php
+++ application/modules/Customer/views/edit_client_user.php
@@ -74,7 +74,7 @@
                         <div class="item form-group">
                             <label class="control-label col-md-3 col-sm-3 col-xs-12">Status <span> *</span></label>
                             <div class="col-md-6 col-sm-6 col-xs-12 control-label" style="text-align:left;">
-                                <input type="checkbox" name="status" id="status" class="flat" <?php echo (isset($client_details[0]->active) && $client_details[0]->active == 1)? $checked : ''; ?>> Active
+                                <input type="checkbox" name="status" id="status" class="flat" <?php echo (isset($client_details[0]->active) && $client_details[0]->active == 1)? 'checked' : ''; ?>> Active
                             </div>
                         </div>					  
                         <div class="ln_solid">
