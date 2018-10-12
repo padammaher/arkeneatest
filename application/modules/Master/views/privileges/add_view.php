0cf28b9e0e2aceef69d9fd5b9e96f094538dd0d1
Swapnil Barhate <swapnil.b@mindworx.in>
2018-10-12 10:52:25 +0530

    application\modules\Customer\views\Edit_customer_info.php

--- application/modules/Master/views/privileges/add_view.php
+++ application/modules/Master/views/privileges/add_view.php
@@ -1,7 +1,7 @@
 <div class="">
     <div class="page-title">
-         <div class="title_left">
-            <h4>Add <?php echo $user_type[0]->rolename ? $user_type[0]->rolename:'';?> Privileges </h4>
+        <div class="title_left">
+            <h4>Add <?php echo $user_type[0]->rolename ? $user_type[0]->rolename : ''; ?> Privileges </h4>
         </div>
 
     </div>
@@ -12,49 +12,52 @@
             <div class="x_panel">
 
                 <div class="x_content" id="privilege-list">
-                    
-                    <form class="form-horizontal form-label-left" action="<?php echo base_url() ?>addPrivileges" method="POST">
-                       <input type="hidden" name="id_and_groupid" value="<?php echo $user_type[0]->group_id ? $user_type[0]->group_id:'';?>_<?php echo $user_type[0]->id ? $user_type[0]->id:'';?>">
-                       
-                    <table id="" class="table table-striped table-bordered item-table" >
-                        <thead>
-                            <tr>
-                                
-                                
-                        <th width="20%">Menu</th>
-                                 <th width="20%" class="text-center">Add</th>
-                                 <th width="20%" class="text-center">Edit</th>
-                                <th width="20%" class="text-center">Delete</th>
-                                  <th width="20%" class="text-center">View</th>
-                               
-                            </tr>
-                        </thead>
-                        <tbody>
-                            
-                            
-                            <?php //echo "<pre>";print_r($user_type[0]);
-                            if (isset($menu) && !empty($menu)) {
-                                foreach ($menu as $k => $data) {
-                                    ?>
-                   <tr>
-                        <td><?php echo $data->menuName ?></td>
-                        <td class="text-center"><label><input class="flat" type="checkbox" value="" id="permission_value" name="permission_add_<?php echo $data->id ?>[]"  <?php if(isset($user_privilege_data[$k]))echo $user_privilege_data[$k]->addpermission =='1'? 'checked':'';?>></label></td>
-                        <td class="text-center"><label><input class="flat" type="checkbox" value="" id="permission_value" name="permission_edit_<?php echo $data->id ?>[]" <?php if(isset($user_privilege_data[$k]))echo $user_privilege_data[$k]->editpermission =='1'? 'checked':'';?>></label></td>
-                        <td class="text-center"><label><input class="flat" type="checkbox" value="" id="permission_value" name="permission_delete_<?php echo $data->id ?>[]" <?php if(isset($user_privilege_data[$k]))echo $user_privilege_data[$k]->deletepermission =='1'? 'checked':'';?>></label></td>
-                        <td class="text-center"><label><input class="flat" type="checkbox" value="" id="permission_value" name="permission_view_<?php echo $data->id ?>[]" <?php if(isset($user_privilege_data[$k]))echo $user_privilege_data[$k]->viewpermission =='1'? 'checked':'';?>></label></td>
 
-                    </tr>
-                            <?php } } 
-                            
-                            else {
-                                ?>
-                            <td colspan="5">No data found..!</td>       
-                        <?php }
-                        ?>
-                            
-                        </tbody>
-                    </table>
-                              
+                    <form class="form-horizontal form-label-left" action="<?php echo base_url() ?>addPrivileges" method="POST">
+                        <input type="hidden" name="id_and_groupid" value="<?php echo $user_type[0]->group_id ? $user_type[0]->group_id : ''; ?>_<?php echo $user_type[0]->id ? $user_type[0]->id : ''; ?>">
+
+                        <table id="" class="table table-striped table-bordered item-table" >
+                            <thead>
+                                <tr>
+
+
+                                    <th width="20%">Menu</th>
+                                    <th width="20%" class="text-center">Add</th>
+                                    <th width="20%" class="text-center">Edit</th>
+                                    <th width="20%" class="text-center">Delete</th>
+                                    <th width="20%" class="text-center">View</th>
+
+                                </tr>
+                            </thead>
+                            <tbody>
+
+
+                                <?php
+                                //echo "<pre>";print_r($user_type[0]);
+                                if (isset($menu) && !empty($menu)) {
+                                    foreach ($menu as $k => $data) {
+                                        ?>
+                                        <tr>
+                                            <td><?php echo $data->menuName ?></td>
+                                            <td class="text-center"><label><input class="flat" type="checkbox" value="" id="permission_value" name="permission_add_<?php echo $data->id ?>[]"  <?php if (isset($user_privilege_data[$k])) echo $user_privilege_data[$k]->addpermission == '1' ? 'checked' : ''; ?>></label></td>
+                                            <td class="text-center"><label><input class="flat" type="checkbox" value="" id="permission_value" name="permission_edit_<?php echo $data->id ?>[]" <?php if (isset($user_privilege_data[$k])) echo $user_privilege_data[$k]->editpermission == '1' ? 'checked' : ''; ?>></label></td>
+                                            <td class="text-center"><label><input class="flat" type="checkbox" value="" id="permission_value" name="permission_delete_<?php echo $data->id ?>[]" <?php if (isset($user_privilege_data[$k])) echo $user_privilege_data[$k]->deletepermission == '1' ? 'checked' : ''; ?>></label></td>
+                                            <td class="text-center"><label><input class="flat" type="checkbox" value="" id="permission_value" name="permission_view_<?php echo $data->id ?>[]" <?php if (isset($user_privilege_data[$k])) echo $user_privilege_data[$k]->viewpermission == '1' ? 'checked' : ''; ?>></label></td>
+
+                                        </tr>
+                                        <?php
+                                    }
+                                }
+
+                                else {
+                                    ?>
+                                <td colspan="5">No data found..!</td>       
+                            <?php }
+                            ?>
+
+                            </tbody>
+                        </table>
+
                         <div class="item form-group">
                             <div class="col-md-12 col-sm-12 col-xs-12 text-right">
                                 <button type="submit" class="btn btn-md btn-primary">Save</button>
