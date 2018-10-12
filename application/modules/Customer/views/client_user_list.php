8ee88c06c2fa6741dba81636893be67f127c905b
Padam <padamasing.m@mindworx.in>
2018-10-12 19:03:40 +0530

    application\modules\Customer\views\customer_business_location_list.php

--- application/modules/Customer/views/client_user_list.php
+++ application/modules/Customer/views/client_user_list.php
@@ -80,7 +80,7 @@
                             ?>
                             <div id="detailsModal<?php echo $i; ?>" class="modal fade" role="dialog">
                                 <div class="modal-dialog">
-                                    Modal content
+                                    
                                     <div class="modal-content">
                                         <div class="modal-header">
                                             <button type="button" class="close" data-dismiss="modal">&times;
