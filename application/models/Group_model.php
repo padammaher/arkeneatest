675ffb3678e9af33957eaa1d3d3e05269f4a020d
Swapnil Barhate <swapnil.b@mindworx.in>
2018-10-22 09:59:37 +0530

    application\models\Group_model.php

--- application/models/Group_model.php
+++ application/models/Group_model.php
@@ -31,7 +31,7 @@ class Group_model extends MY_Model {
         $this->db->where('id', $id);
         $query = $this->db->get();
         $objData = $query->result_array();
-      //  var_dump($objData);
+        //  var_dump($objData);
         return $objData[0];
     }
 
@@ -47,9 +47,8 @@ class Group_model extends MY_Model {
         return $objData;
     }
 
-    public function load_dashboard_batch_data($user_id)
-    {
-        $batch_data_query=$this->db->select('`batchdata`.`id`,
+    public function load_dashboard_batch_data($user_id) {
+        $batch_data_query = $this->db->select('`batchdata`.`id`,
                                             `batchdata`.`serial_num`,
                                             `batchdata`.`asset_id`,`asset`.`code`,
                                             `asset`.`location_id` as `asset_location_id`,`asset`.`customer_locationid`,
@@ -62,19 +61,17 @@ class Group_model extends MY_Model {
                                             `batchdata`.`p5`,
                                             `batchdata`.`runcode_id`,
                                             `batchdata`.`createddate`')
-                                    ->from('batchdata')
-                                    ->join('runcode','batchdata.runcode_id=runcode.id','inner')
-                                    ->join('asset','asset.id= batchdata.asset_id','inner')
-                                    ->where('batchdata.runcode_id =(select id from runcode order by runcode.id desc limit 1)',NULL, FALSE)
-                                    ->get();
+                ->from('batchdata')
+                ->join('runcode', 'batchdata.runcode_id=runcode.id', 'inner')
+                ->join('asset', 'asset.id= batchdata.asset_id', 'inner')
+                ->where('batchdata.runcode_id =(select id from runcode order by runcode.id desc limit 1)', NULL, FALSE)
+                ->get();
 
-        return $batch_data_result=$batch_data_query->result_array();
-
+        return $batch_data_result = $batch_data_query->result_array();
     }
 
-    function get_data_by_assets($asset_id, $location_id, $user_id)
-    {
-      $group_id = $this->session->userdata('group_id');
+    function get_data_by_assets($asset_id, $location_id, $user_id) {
+        $group_id = $this->session->userdata('group_id');
         $this->db->select('device_asset.id,
                            device_asset.device_id,
                            device_asset.asset_id,
@@ -84,32 +81,33 @@ class Group_model extends MY_Model {
         $this->db->from('device_inventory');
         $this->db->join('device_asset', 'device_asset.device_id=device_inventory.id');
         $this->db->join('asset', 'asset.id=device_asset.asset_id');
-        
-        $this->db->where(array('device_asset.asset_id'=>$asset_id,'device_asset.isdeleted'=> 0));
-         if($group_id =='2'){
 
-         // $this->db->where('device_inventory.customer_location_id',$location_id);
+        $this->db->where(array('device_asset.asset_id' => $asset_id, 'device_asset.isdeleted' => 0));
+        if ($group_id == '2') {
+
+            // $this->db->where('device_inventory.customer_location_id',$location_id);
         }
 
         $query = $this->db->get();
-       // echo $this->db->last_query();        
+        // echo $this->db->last_query();        
         $data['objData'] = $query->result_array();
 
 
-        $query2="select parameter.id,parameter_range.min_value,parameter_range.max_value,
+        $query2 = "select parameter.id,parameter_range.min_value,parameter_range.max_value,parameter.name,uom.name as uom,
                 (CASE 
-                  WHEN parameter.id ='1' THEN (select batchdata.p1 from batchdata where batchdata.asset_id='6' order by batchdata.id desc limit 1)  
-                  WHEN parameter.id ='2' THEN (select batchdata.p2 from batchdata where batchdata.asset_id='6' order by batchdata.id desc limit 1)  
-                  WHEN parameter.id ='3' THEN (select batchdata.p3 from batchdata where batchdata.asset_id='6' order by batchdata.id desc limit 1)  
-                  WHEN parameter.id ='4' THEN (select batchdata.p4 from batchdata where batchdata.asset_id='6' order by batchdata.id desc limit 1)  
-                  WHEN parameter.id ='5' THEN (select batchdata.p5 from batchdata where batchdata.asset_id='6' order by batchdata.id desc limit 1)  
+                  WHEN parameter.id ='1' THEN (select batchdata.p1 from batchdata where batchdata.asset_id='" . $asset_id . "' order by batchdata.id desc limit 1)  
+                  WHEN parameter.id ='2' THEN (select batchdata.p2 from batchdata where batchdata.asset_id='" . $asset_id . "' order by batchdata.id desc limit 1)  
+                  WHEN parameter.id ='3' THEN (select batchdata.p3 from batchdata where batchdata.asset_id='" . $asset_id . "' order by batchdata.id desc limit 1)  
+                  WHEN parameter.id ='4' THEN (select batchdata.p4 from batchdata where batchdata.asset_id='" . $asset_id . "' order by batchdata.id desc limit 1)  
+                  WHEN parameter.id ='5' THEN (select batchdata.p5 from batchdata where batchdata.asset_id='" . $asset_id . "' order by batchdata.id desc limit 1)  
                   ELSE 0
                 END) AS 'current_value'
                 from parameter
                 inner join parameter_range on parameter.id = parameter_range.parameter_id
+                inner join uom on parameter_range.uom_id=uom.id
                 group by parameter.id";
 
-        $data['chart_data']=$this->db->query($query2)->result_array();
+        $data['chart_data'] = $this->db->query($query2)->result_array();
         return $data;
     }
 
