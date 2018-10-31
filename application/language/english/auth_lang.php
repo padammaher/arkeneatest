2e2c23a087f76b4528efa98b446578f024c2b29a
Pradnya <pradnya.w@mindworx.in>
2018-10-31 12:09:29 +0530

    application\language\english\auth_lang.php application\language\english\ion_auth_lang.php

--- application/language/english/auth_lang.php
+++ application/language/english/auth_lang.php
@@ -151,7 +151,7 @@ $lang['forgot_password_submit_btn']              = 'Submit';
 $lang['forgot_password_validation_email_label']  = 'Email Address';
 $lang['forgot_password_identity_label'] = 'Identity';
 $lang['forgot_password_email_identity_label']    = 'Email';
-$lang['forgot_password_email_not_found']         = 'No record of that email address.';
+$lang['forgot_password_email_not_found']         = 'This Email Id doesn\'t exists. Please enter differnt email id.!';
 $lang['forgot_password_identity_not_found']         = 'No record of that username.';
 
 // Reset Password
